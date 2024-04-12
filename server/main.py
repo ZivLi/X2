4ea52b612d69f0d439340621a8ac8080960aba65
ZivLi <zivli23@163.com>
2024-04-12 12:03:37 +0800

    add websocket methods.

---
+++ server/main.py
@@ -0,0 +1,84 @@
+import threading
+import json
+from flask_socketio import SocketIO
+from flask import Flask, request
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+class TimerList:
+    def __init__(self) -> None:
+        self.l = []
+
+    def append(self, v):
+        idx = len(self.l)
+        self.l.append(v)
+        threading.Timer(3660, self.remove, args=(v,)).start()
+
+    def remove(self, v):
+        self.l.remove(v)
+
+    def __iter__(self):
+        self._idx = 0
+        return self
+
+    def __next__(self):
+        if self._idx < len(self.l):
+            v = self.l[self._idx]
+            self._idx += 1
+            return v
+        else:
+            raise StopIteration
+
+
+shop_users = {"s1": TimerList()}
+for u in [
+    {
+        "avatarUrl": "https://gravatar.com/avatar/e352769376aed6577e8142f7cbd518db?s=400&d=robohash&r=x",
+        "nickName": "tom",
+    },
+    {
+        "avatarUrl": "https://gravatar.com/avatar/5ead1bfde756bd693435fde2e39631e3?s=400&d=robohash&r=x",
+        "nickName": "pop",
+    },
+    {
+        "avatarUrl": "https://robohash.org/8b5e38be4eb8e5204686a9ff10b91acb?set=set4&bgset=&size=400x400",
+        "nickName": "ax",
+    },
+    {
+        "avatarUrl": "https://robohash.org/f02d530fa285506c2ea14919882914f4?set=set4&bgset=&size=400x400",
+        "nickName": "cat",
+    },
+]:
+    shop_users["s1"].append(u)
+
+
+@socketio.on("connect")
+def handle_connect():
+    print("----connect websocket")
+
+
+@app.route("/shop_users")
+def get_shop_users():
+    shop_id = request.args.get("shop")
+    return list(shop_users.get(shop_id, TimerList()))
+
+
+@app.route("/join_shop", methods=["POST"])
+def post_join_shop_users():
+    shop_id = request.args.get("shop")
+    shop_users.get(shop_id, TimerList()).append(request.json)
+
+    socketio.emit(shop_id, list(shop_users[shop_id]))
+    return list(shop_users[shop_id])
+
+
+@app.route("/test")
+def test_ws():
+    socketio.emit("e1", json.dumps({"a": "1", "b": 23}))
+    return "wer"
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=8086, debug=True, use_reloader=True)
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,49 +1,219 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view>
-			<text class="title">{{title}}</text>
+	<view class="container">
+		<input v-model="searchKeyword" type="text" placeholder="输入店铺名进行筛选" />
+		<button type="primary" @tap="searchNearbyShops">搜索</button>
+		<view v-if="shops.length > 0">
+			<view v-for="(shop, index) in shops" :key="index" @tap="selectShop(shop)">
+				<text>{{ shop.name }}</text>
+			</view>
+		</view>
+
+		<view class="group-container">
+			<p class="row_p">
+				<view v-for="(row, rowIndex) in 2" :key="rowIndex" class="row">
+					<view v-for="(item, idx) in getRowItems(rowIndex)" :key="idx" class="item">
+						<view v-if="item.type == 'user'" @tap="selectItem(item)" class="user-item">
+							<image class="avatar" :src="item.user.avatarUrl" mode="scaleToFill" />
+							<text class="nickname">{{ item.user.nickName }}</text>
+						</view>
+						<view v-else class="empty-item">
+							<image class="avatar" src="../../static/plus-sign.png" mode="scaleToFill" />
+						</view>
+					</view>
+				</view>
+			</p>
+		</view>
+
+		<view class="button-container">
+			<button class="btn" type="primary" @tap="joinGroup">加入组团</button>
+			<button class="btn" type="primary" @tap="joinGroup">退出组团</button>
 		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
+import io from '@hyoga/uni-socket.io'
+import './index.css'
+
+export default {
+	data() {
+		return {
+			serverUrl: 'http://localhost:8086',
+			wsServerUrl: 'ws://localhost:8086',
+			socket: null,
+			rowCount: 3,
+			users: [],
+			userInfo: null,
+			searchKeyword: '',
+			userLocation: null,
+			shops: [] // 附近店铺列表
+		};
+	},
+	computed: {
+		getRowItems() {
+			return rowIndex => {
+				const start = rowIndex * 3;
+				const end = start + 3;
+				return this.displayItems.slice(start, end);
 			}
 		},
-		onLoad() {
+		displayItems() {
+			const displayItems = [];
+			const totalItems = 3 * 3;
 
-		},
-		methods: {
-
+			for (let i = 0; i < totalItems; i++) {
+				if (i < this.users.length) {
+					displayItems.push({ type: 'user', user: this.users[i] });
+				} else {
+					displayItems.push({ type: 'empty' })
+				}
+			}
+			return displayItems
 		}
+	},
+	methods: {
+		getUsers() {
+			uni.request({
+				url: this.serverUrl + '/shop_users',
+				method: 'GET',
+				data: { shop: 's1' }
+				, success: (res) => {
+					if (res.statusCode === 200) {
+						this.users = res.data
+					} else {
+						console.error('get users failed')
+					}
+				}, fail: (err) => {
+					console.error('request failed.')
+				}
+			})
+		},
+		joinGroup() {
+			this.getUserInfo().then(() => {
+				uni.request({
+					url: this.serverUrl + '/join_shop?shop=' + 's1',
+					method: 'POST',
+					data: {
+						avatarUrl: this.userInfo.avatarUrl,
+						nickName: this.userInfo.nickName
+					},
+					header: {
+						'Content-Type': 'application/json'
+					},
+					success: (res) => {
+						if (res.data.length >= 3) {
+							this.showGroupSuccessModal()
+						}
+						console.log('join success')
+					},
+				});
+			}).catch((err) => { console.log(err) })
+		},
+		addUser(index) {
+			console.log(index)
+		},
+
+		showGroupSuccessModal() {
+			uni.showModal({
+				title: '组团成功',
+				content: '快去收银台集结吧！',
+				showCancel: false,
+				confirmText: '知道了',
+				success: (res) => {
+					// 弹窗关闭时触发的回调函数
+					if (res.confirm) {
+						console.log('用户点击了知道了');
+					}
+				}
+			})
+		},
+		getUserInfo() {
+			return new Promise((resolve, reject) => {
+				uni.getUserInfo({
+					success: (res) => {
+						this.userInfo = res.userInfo,
+							resolve();
+					},
+					fail: (err) => {
+						console.error("get user inf failed")
+						reject(err)
+					}
+				})
+			})
+		},
+		selectItem(item) {
+			if (item.type == 'user') {
+				console.log('choose', item.user.nickName)
+			} else {
+				console.log('run here')
+			}
+		},
+		// 获取用户地理位置
+		getUserLocation() {
+			wx.getLocation({
+				success: (res) => {
+					this.userLocation = res;
+				},
+				fail: (res) => {
+					console.error(res)
+					console.error('获取用户地理位置失败');
+				}
+			});
+		},
+		connectWebsocket() {
+			const socket = io(this.wsServerUrl, {
+				transports: ['websocket', 'polling'],
+			});
+			socket.on('connect', () => {
+				console.log('connected')
+
+				socket.on('s1', (message) => {
+					this.userInfo = message
+				})
+			})
+		},
+		// 根据用户输入的关键词搜索附近的店铺
+		searchNearbyShops() {
+			if (!this.userLocation) {
+				console.error("请先获取用户地理位置");
+				return;
+			}
+
+			wx.request({
+				url: 'https://apis.map.qq.com/ws/place/v1/search',
+				data: {
+					keyword: this.searchKeyword,
+					boundary: `nearby(${this.userLocation.latitude},${this.userLocation.longitude},5000)`,
+					key: 'YOUR_TENCENT_MAP_API_KEY',
+					output: 'json'
+				},
+				success: (res) => {
+					if (res.data.status === 0) {
+						this.shops = res.data.data;
+					} else {
+						console.error('搜索附近店铺失败：', res.data.message);
+					}
+				},
+				fail: (error) => {
+					console.error('搜索附近店铺失败：', error);
+				}
+			})
+
+			// 使用地图API根据关键词搜索附近的店铺，并更新this.shops
+			// 比如：调用腾讯地图API
+		},
+		// 选中店铺
+		selectShop(shop) {
+
+			// 用户选中店铺后的操作，比如跳转到店铺详情页等
+		}
+	},
+	mounted() {
+		this.connectWebsocket();
+		//   this.getUserLocation(); // 组件加载时获取用户地理位置
+	},
+	onLoad() {
+		this.getUsers()
 	}
+};
 </script>
-
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
-</style>
